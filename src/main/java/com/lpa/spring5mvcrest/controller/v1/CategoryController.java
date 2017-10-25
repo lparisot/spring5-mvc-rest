a28dd59466dcaf0883a238ff86730bfad9b01011
L.P <parisot.laurent@gmail.com>
2017-10-25 09:37:16 +0200

    List categories

--- src/main/java/com/lpa/spring5mvcrest/controller/v1/CategoryController.java
+++ src/main/java/com/lpa/spring5mvcrest/controller/v1/CategoryController.java
@@ -1,7 +1,13 @@
 package com.lpa.spring5mvcrest.controller.v1;
 
+import com.lpa.spring5mvcrest.api.v1.model.CategoryDTO;
+import com.lpa.spring5mvcrest.api.v1.model.CategoryListDTO;
 import com.lpa.spring5mvcrest.services.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +17,21 @@ public class CategoryController {
 
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<CategoryListDTO> getAllCategories() {
+        return new ResponseEntity<CategoryListDTO>(
+                new CategoryListDTO(categoryService.getAllCategories()),
+                HttpStatus.OK
+        );
+    }
+
+    @GetMapping("{name}")
+    public ResponseEntity<CategoryDTO> getCategoryByName(@PathVariable String name) {
+        return new ResponseEntity<CategoryDTO>(
+                categoryService.getCategoryByName(name),
+                HttpStatus.OK
+        );
     }
 }
