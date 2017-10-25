b51acf14093f298f186183553e2a7bc616ab768b
L.P <parisot.laurent@gmail.com>
2017-10-25 11:33:48 +0200

    Customer service

--- src/main/java/com/lpa/spring5mvcrest/services/CustomerServiceImpl.java
+++ src/main/java/com/lpa/spring5mvcrest/services/CustomerServiceImpl.java
@@ -5,6 +5,7 @@ import com.lpa.spring5mvcrest.api.v1.model.CustomerDTO;
 import com.lpa.spring5mvcrest.repositories.CustomerRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CustomerServiceImpl implements CustomerService {
     private final CustomerMapper customerMapper;
@@ -17,11 +18,15 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public List<CustomerDTO> getAllCustomers() {
-        return null;
+        return customerRepository
+                .findAll()
+                .stream()
+                .map(customerMapper::customerToCustomerDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
     public CustomerDTO getCustomerByLastName(String name) {
-        return null;
+        return customerMapper.customerToCustomerDTO(customerRepository.findByLastName(name));
     }
 }
