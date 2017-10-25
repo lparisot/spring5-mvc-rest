222bb95b403b3e5a771644b9610f6fda9bd100e5
L.P <parisot.laurent@gmail.com>
2017-10-25 12:03:30 +0200

    Bootstrap some customers

--- src/main/java/com/lpa/spring5mvcrest/bootstrap/Bootstrap.java
+++ src/main/java/com/lpa/spring5mvcrest/bootstrap/Bootstrap.java
@@ -1,16 +1,20 @@
 package com.lpa.spring5mvcrest.bootstrap;
 
 import com.lpa.spring5mvcrest.domain.Category;
+import com.lpa.spring5mvcrest.domain.Customer;
 import com.lpa.spring5mvcrest.repositories.CategoryRepository;
+import com.lpa.spring5mvcrest.repositories.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Bootstrap implements CommandLineRunner {
     private CategoryRepository categoryRepository;
+    private CustomerRepository customerRepository;
 
-    public Bootstrap(CategoryRepository categoryRepository) {
+    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository) {
         this.categoryRepository = categoryRepository;
+        this.customerRepository = customerRepository;
     }
 
     @Override
@@ -35,6 +39,28 @@ public class Bootstrap implements CommandLineRunner {
         categoryRepository.save(fresh);
         categoryRepository.save(exotic);
         categoryRepository.save(nuts);
+
+        Customer meyers = new Customer();
+        meyers.setFirstname("Freddy");
+        meyers.setLastname("Meyers");
+
+        Customer buck = new Customer();
+        buck.setFirstname("Joe");
+        buck.setLastname("Buck");
+
+        Customer weston = new Customer();
+        weston.setFirstname("Michael");
+        weston.setLastname("Weston");
+
+        Customer doe = new Customer();
+        doe.setFirstname("John");
+        doe.setLastname("Doe");
+
+        customerRepository.save(meyers);
+        customerRepository.save(buck);
+        customerRepository.save(weston);
+        customerRepository.save(doe);
+
 
         System.out.println("Data loaded = " + categoryRepository.count());
     }
